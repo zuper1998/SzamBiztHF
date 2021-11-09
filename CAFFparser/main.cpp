126a17a790be67f6897c9db3dfebba7a218315ee
zuper1998 <andras.mihaly@edu.bme.hu>
2021-11-09 18:25:04 +0100

    Making Caff parser more presentable

--- CAFFparser/main.cpp
+++ CAFFparser/main.cpp
@@ -2,10 +2,14 @@
 #include "Parsers/CAFFparser.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    CAFF c = CAFFparser::parser("TestFiles/3.caff");
-    //c.print();
-    std::cout << "HELL YEA!" << std::endl;
+    std::cout << "Loading TestFile 1." << std::endl;
+    CAFF c1 = CAFFparser::parser("TestFiles/1.caff");
+    std::cout << "Loading TestFile 2." << std::endl;
+    CAFF c2 = CAFFparser::parser("TestFiles/2.caff");
+    std::cout << "Loading TestFile 3." << std::endl;
+    CAFF c3 = CAFFparser::parser("TestFiles/3.caff");
+    //c.print(); -- Only use it to get the pictures out of the CAFF files for testing
+    std::cout << "Succsessfully loaded all TestFiles!" << std::endl;
 
     return 0;
 }
