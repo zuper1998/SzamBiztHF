7a3c1f28cc053cb5e50fda85019c11feccf7c31d
lyaflora <petrok.flora@gmail.com>
2021-12-05 00:42:25 +0100

    Add login and registration tests for UserController

--- backend/backend/src/test/java/com/backend/backend/Controller/UserControllerTests.java
+++ backend/backend/src/test/java/com/backend/backend/Controller/UserControllerTests.java
@@ -1,7 +1,11 @@
 package com.backend.backend.Controller;
 
+import com.backend.backend.Communication.Request.LoginRequest;
 import com.backend.backend.Communication.Request.SignUpRequest;
 import com.backend.backend.Communication.Request.UpdateUserRequest;
+import com.backend.backend.Data.Role;
+import com.backend.backend.Data.RoleEnum;
+import com.backend.backend.Data.User;
 import com.backend.backend.Repository.LogRepository;
 import com.backend.backend.Repository.RoleRepository;
 import com.backend.backend.Repository.UserRepository;
@@ -11,12 +15,17 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.test.context.support.WithMockUser;
 
+import java.util.ArrayList;
+import java.util.Set;
 import java.util.UUID;
 
 @SpringBootTest
@@ -56,6 +65,113 @@ public class UserControllerTests {
     @Test
     public void controllerLoads() {
         Assertions.assertNotNull(userController);
+    }
+
+    @WithMockUser(roles = { "ADMIN" }, username = "admin", password = "admin")
+    @Test
+    public void loginAdminBadCredentials() {
+        LoginRequest loginRequest = new LoginRequest();
+        loginRequest.setUsername("user");
+        loginRequest.setPassword("user");
+        Assertions.assertNotEquals("admin", loginRequest.getUsername());
+        Assertions.assertNotEquals("admin", loginRequest.getPassword());
+        Assertions.assertThrows(BadCredentialsException.class, () -> userController.loginAdmin(loginRequest));
+    }
+
+    @WithMockUser(roles = { "ADMIN" }, username = "admin", password = "admin")
+    @Test
+    public void loginAdminGoodCredentials() {
+        LoginRequest loginRequest = new LoginRequest();
+        loginRequest.setUsername("admin");
+        loginRequest.setPassword("admin");
+        Assertions.assertEquals("admin", loginRequest.getUsername());
+        Assertions.assertEquals("admin", loginRequest.getPassword());
+        Assertions.assertDoesNotThrow(() -> userController.loginAdmin(loginRequest));
+    }
+
+    @WithMockUser(roles = { "USER" }, username = "admin", password = "admin")
+    @Test
+    public void loginUserBadCredentials() {
+        LoginRequest loginRequest = new LoginRequest();
+        loginRequest.setUsername("user");
+        loginRequest.setPassword("user");
+        Assertions.assertNotEquals("admin", loginRequest.getUsername());
+        Assertions.assertNotEquals("admin", loginRequest.getPassword());
+        Assertions.assertThrows(BadCredentialsException.class, () -> userController.loginUser(loginRequest));
+    }
+
+    @WithMockUser(roles = { "USER" }, username = "admin", password = "admin")
+    @Test
+    public void loginUserGoodCredentials() {
+        LoginRequest loginRequest = new LoginRequest();
+        loginRequest.setUsername("admin");
+        loginRequest.setPassword("admin");
+        Assertions.assertEquals("admin", loginRequest.getUsername());
+        Assertions.assertEquals("admin", loginRequest.getPassword());
+        Assertions.assertDoesNotThrow(() -> userController.loginUser(loginRequest));
+    }
+
+    @Test
+    public void registrationNewCredentials() {
+        SignUpRequest signUpMessage = new SignUpRequest();
+        String newUserName = "newuser";
+        String newEmail = "new@new.com";
+        String newPassword = "newpassword";
+        User newUser = new User(newUserName, newEmail, encoder.encode(newPassword), new ArrayList<>(), new ArrayList<>());
+        newUser.setRoles(Set.of(new Role(RoleEnum.ROLE_USER)));
+        signUpMessage.setUsername(newUserName);
+        signUpMessage.setEmail(newEmail);
+        signUpMessage.setPassword(newPassword);
+        ResponseEntity<Void> responseEntity = userController.registration(signUpMessage);
+        Assertions.assertEquals("newuser", newUser.getUsername());
+        Assertions.assertEquals("new@new.com", newUser.getEmail());
+        Assertions.assertTrue(encoder.matches("newpassword", newUser.getPassword()));
+        Assertions.assertEquals("newuser", signUpMessage.getUsername());
+        Assertions.assertEquals("new@new.com", signUpMessage.getEmail());
+        Assertions.assertEquals("newpassword", signUpMessage.getPassword());
+        Assertions.assertTrue(newUser.getRoles().stream().anyMatch(role -> role.getName().equals(RoleEnum.ROLE_USER)));
+        Assertions.assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+    }
+
+    @Test
+    public void registrationExistingCredentials() {
+        SignUpRequest signUpMessage = new SignUpRequest();
+        String newUserName = "existing";
+        String newEmail = "new@new.com";
+        String newPassword = "existing";
+        User newUser = new User(newUserName, newEmail, encoder.encode(newPassword), new ArrayList<>(), new ArrayList<>());
+        newUser.setRoles(Set.of(new Role(RoleEnum.ROLE_USER)));
+        signUpMessage.setUsername(newUserName);
+        signUpMessage.setEmail(newEmail);
+        signUpMessage.setPassword(newPassword);
+        ResponseEntity<Void> responseEntity = userController.registration(signUpMessage);
+        Assertions.assertEquals("existing", newUser.getUsername());
+        Assertions.assertEquals("new@new.com", newUser.getEmail());
+        Assertions.assertTrue(encoder.matches("existing", newUser.getPassword()));
+        Assertions.assertEquals("existing", signUpMessage.getUsername());
+        Assertions.assertEquals("new@new.com", signUpMessage.getEmail());
+        Assertions.assertEquals("existing", signUpMessage.getPassword());
+        Assertions.assertTrue(newUser.getRoles().stream().anyMatch(role -> role.getName().equals(RoleEnum.ROLE_USER)));
+        Assertions.assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+
+        SignUpRequest signUpMessage2 = new SignUpRequest();
+        String newUserName2 = "existing";
+        String newEmail2 = "new@new.com";
+        String newPassword2 = "existing";
+        User newUser2 = new User(newUserName2, newEmail2, encoder.encode(newPassword2), new ArrayList<>(), new ArrayList<>());
+        newUser2.setRoles(Set.of(new Role(RoleEnum.ROLE_USER)));
+        signUpMessage2.setUsername(newUserName2);
+        signUpMessage2.setEmail(newEmail2);
+        signUpMessage2.setPassword(newPassword2);
+        ResponseEntity<Void> responseEntity2 = userController.registration(signUpMessage2);
+        Assertions.assertEquals("existing", newUser2.getUsername());
+        Assertions.assertEquals("new@new.com", newUser2.getEmail());
+        Assertions.assertTrue(encoder.matches("existing", newUser2.getPassword()));
+        Assertions.assertEquals("existing", signUpMessage2.getUsername());
+        Assertions.assertEquals("new@new.com", signUpMessage2.getEmail());
+        Assertions.assertEquals("existing", signUpMessage2.getPassword());
+        Assertions.assertTrue(newUser2.getRoles().stream().anyMatch(role -> role.getName().equals(RoleEnum.ROLE_USER)));
+        Assertions.assertEquals(HttpStatus.BAD_REQUEST, responseEntity2.getStatusCode());
     }
 
     @Test
