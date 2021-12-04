16c0655d9892c1f188c469f000e13d67ce9fbf4b
makkbalazs <makkbalazs@gmail.com>
2021-12-04 18:46:58 +0100

    Started creating tests for CaffController.

--- backend/backend/src/test/java/com/backend/backend/Controller/CaffControllerTests.java
+++ backend/backend/src/test/java/com/backend/backend/Controller/CaffControllerTests.java
@@ -1,14 +1,23 @@
 package com.backend.backend.Controller;
 
+import com.backend.backend.Communication.Request.SearchRequest;
 import com.backend.backend.Repository.CaffRepository;
 import com.backend.backend.Repository.LogRepository;
 import com.backend.backend.Repository.UserRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.web.multipart.MultipartFile;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import java.io.IOException;
+import java.util.UUID;
 
+@ContextConfiguration
 @SpringBootTest
 public class CaffControllerTests {
 
@@ -24,13 +33,49 @@ public class CaffControllerTests {
 
     @Test
     public void contextLoads() {
-        assertNotNull(ur);
-        assertNotNull(cr);
-        assertNotNull(lr);
+        Assertions.assertNotNull(ur);
+        Assertions.assertNotNull(cr);
+        Assertions.assertNotNull(lr);
     }
 
     @Test
     public void controllerLoads() {
-        assertNotNull(caffController);
+        Assertions.assertNotNull(caffController);
+    }
+
+    @Test
+    public void addCaffUnauthenticated() {
+        MultipartFile caffFile = new MockMultipartFile("testFile", "testData".getBytes());
+        String title = "testTitle";
+        Assertions.assertThrows(AuthenticationCredentialsNotFoundException.class, () -> caffController.addCaff(caffFile, title));
+    }
+
+    @WithMockUser(roles = { "USER" })
+    @Test
+    public void addCaffAuthenticated() {
+        MultipartFile caffFile = new MockMultipartFile("testFile", "testData".getBytes());
+        String title = "testTitle";
+        /*try {
+            caffController.addCaff(caffFile, title);
+        } catch (IOException e) {
+            Assertions.fail(e.getMessage() + '\n' + e.getStackTrace());
+        }*/
+    }
+
+    @Test
+    public void getCaffLikeUnauthenticated() {
+        SearchRequest searchRequest = new SearchRequest();
+        Assertions.assertThrows(AuthenticationCredentialsNotFoundException.class, () -> caffController.getCaffLike(searchRequest));
+    }
+
+    @Test
+    public void downloadCaffUnauthenticated() {
+        UUID uuid = new UUID(0, 1);
+        Assertions.assertThrows(AuthenticationCredentialsNotFoundException.class, () -> caffController.downloadCaff(uuid));
+    }
+
+    @Test
+    public void getAllCaffUnauthorized() {
+        Assertions.assertThrows(AuthenticationCredentialsNotFoundException.class, () -> caffController.getAllCaff());
     }
 }
